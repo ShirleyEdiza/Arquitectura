e4ae5e6d0b3e4a05f3cc45959ff3dbfe2c5855a6
Liliana <lilianapaguay15@gmail.com>
2025-11-11 13:24:27 -0500

    FEAT: Nuevos cambios en el frontend

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 // public/js/config.js - CONFIGURACIÓN CORREGIDA
 window.APP_CONFIG = {
     // ✅ APUNTAR AL BACKEND, no al frontend
-    API_BASE_URL: 'http://192.168.69.134:3002/api/v1',
-    SOCKET_URL: 'http://192.168.69.134:3002'
+    API_BASE_URL: 'http://192.168.239.134:3002/api/v1',
+    SOCKET_URL: 'http://192.168.239.134:3002'
 };
 
 console.log('✅ Configuración cargada:', window.APP_CONFIG);
